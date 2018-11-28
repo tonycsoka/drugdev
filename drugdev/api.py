4432299468ba6e17b7d91584fe479365a773171a
tony <tony.csoka@gmail.com>
2018-11-28 22:01:06 +0000

    Changes for Part 2

--- drugdev/api.py
+++ drugdev/api.py
@@ -1,5 +1,5 @@
 from drugdev import app, db
-from drugdev.models import Contact, ContactSchema
+from drugdev.models import Contact, ContactSchema, Email, EmailSchema
 from flask_restful import Resource, Api, reqparse
 
 api = Api(app=app)
@@ -20,27 +20,37 @@ class ContactCall(Resource):
         contact = Contact.query.filter_by(username=username).first()
         contacts_schema = ContactSchema()
 
+        print(username)
+
         if contact:
             return {'/api/contact/'+username: contacts_schema.dump(contact).data}
         else:
-            return {'/api/contact/'+username:  'unknown'}, 204
+            email = Email.query.filter_by(email=username).first()
+            if email:
+                contact = Contact.query.filter_by(id=email.contact_id).first()
+                return {'/api/contact/'+contact.username: contacts_schema.dump(contact).data}
+            else:
+                return {'/api/contact/'+username:  'unknown'}, 204
 
     def post(self, username):
         from sqlalchemy.exc import IntegrityError
         parser = reqparse.RequestParser()
         parser.add_argument('email')
-        parser.add_argument('last_name')
+        parser.add_argument('surname')
         parser.add_argument('first_name')
 
         args = parser.parse_args()
 
         contact = Contact(username=username,
-                          email=args['email'],
                           first_name=args['first_name'],
-                          last_name=args['last_name'])
+                          surname=args['surname'])
+        email = Email(email=args['email'])
+
+        contact.emails.append(email)
 
         try:
             db.session.add(contact)
+            db.session.add(email)
             db.session.commit()
         except IntegrityError as ierror:
             db.session.rollback()
@@ -54,7 +64,7 @@ class ContactCall(Resource):
         from sqlalchemy.exc import IntegrityError
         parser = reqparse.RequestParser()
         parser.add_argument('email')
-        parser.add_argument('last_name')
+        parser.add_argument('surname')
         parser.add_argument('first_name')
         parser.add_argument('username')
 
@@ -66,8 +76,8 @@ class ContactCall(Resource):
             contact.username = args['username']
         if args['email']:
             contact.email = args['email']
-        if args['last_name']:
-            contact.last_name = args['last_name']
+        if args['surname']:
+            contact.surname = args['surname']
         if args['first_name']:
             contact.first_name = args['first_name']
 
