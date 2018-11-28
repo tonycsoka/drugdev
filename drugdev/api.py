680d4cf99724e8f80e5d145ca22ca3edb4a0e3e1
tony <tony.csoka@gmail.com>
2018-11-28 01:36:57 +0000

    tests for save and update

--- drugdev/api.py
+++ drugdev/api.py
@@ -1,6 +1,6 @@
 from drugdev import app, db
 from drugdev.models import Contact, ContactSchema
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 api = Api(app=app)
 
@@ -23,19 +23,62 @@ class ContactCall(Resource):
         if contact:
             return {'/api/contact/'+username: contacts_schema.dump(contact).data}
         else:
-            return {'/api/contact/'+username:  'unknown'}, 403
+            return {'/api/contact/'+username:  'unknown'}, 204
 
     def post(self, username):
+        from sqlalchemy.exc import IntegrityError
+        parser = reqparse.RequestParser()
+        parser.add_argument('email')
+        parser.add_argument('last_name')
+        parser.add_argument('first_name')
+
+        args = parser.parse_args()
+
+        contact = Contact(username=username,
+                          email=args['email'],
+                          first_name=args['first_name'],
+                          last_name=args['last_name'])
+
+        try:
+            db.session.add(contact)
+            db.session.commit()
+        except IntegrityError as ierror:
+            db.session.rollback()
+            return {'/api/contact/'+username: 'already exists'}, 405
+        finally:
+            db.session.commit()
+
         return f'post : {username}', 201
 
     def put(self, username):
+        parser = reqparse.RequestParser()
+        parser.add_argument('email')
+        parser.add_argument('last_name')
+        parser.add_argument('first_name')
+        parser.add_argument('username')
+
+        contact = Contact.query.filter_by(username=username).first()
+
+        args = parser.parse_args()
+
+        if args['username']:
+            contact.username = args['username']
+        if args['email']:
+            contact.email = args['email']
+        if args['last_name']:
+            contact.last_name = args['last_name']
+        if args['first_name']:
+            contact.first_name = args['first_name']
+
+        db.session.commit()
+
         return f'put : {username}', 201
 
     def delete(self, username):
         contact = Contact.query.filter_by(username=username)
         contact.delete()
         db.session.commit()
-        return '', 204
+        return '', 200
 
 
 api.add_resource(ContactCall, '/api/contact/<string:username>')
--- tests/test_api.py
+++ tests/test_api.py
@@ -19,4 +19,32 @@ def test_get_contact_pass(client, add_user):
 
 
 def test_get_contact_fail(client, add_user):
-    assert client.get('/api/contact/fred').status_code == 403
+    assert client.get('/api/contact/fred').status_code == 204
+
+
+def test_delete_contact(client, add_user):
+    assert client.get('/api/contact/bob').status_code == 200
+    assert client.delete('/api/contact/bob').status_code == 200
+    assert client.get('/api/contact/bob').status_code == 204
+
+
+def test_save_contact(client):
+    data = {'email': 'jim@jim.com', 'last_name': 'greeve', 'first_name': 'jim'}
+    client.post('/api/contact/jim', json=data)
+    rv = client.get('/api/contact/jim')
+    assert rv.status_code == 200
+
+
+def test_save_contact_fail(client, add_user):
+    data = {'email': 'jim@jim.com', 'last_name': 'greeve', 'first_name': 'jim'}
+    # Insert of new user will fail, as we already have bob as a username
+    assert client.post('/api/contact/bob', json=data).status_code == 405
+    # Make sure old bob is still there
+    assert client.get('/api/contact/bob').status_code == 200
+
+
+def test_update_user(client, add_user):
+    data = {'last_name': 'mink'}
+    client.put('/api/contact/bob', json=data)
+    rv = client.get('/api/contact/bob').get_json()
+    assert rv['/api/contact/bob']['last_name'] == 'mink'
