254ea795abff6a3fa2b03de0b1cebe8f7fc22cb2
tony <tony.csoka@gmail.com>
2018-11-29 20:01:38 +0000

    Part 3 - add celery tasks

--- drugdev/__init__.py
+++ drugdev/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
+from celery import Celery
 
 config_name = os.getenv('FLASK_ENV', 'default')
 
@@ -13,7 +14,4 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 migrate = Migrate(app, db)
 
-#from drugdev import models
 from drugdev.api import api
-
-
